57f898f3f290568ffd31461d41b63345da575309
Carrie Miskin <clove40@gmail.com>
2022-03-26 21:14:44 -0400

    commit tree 37c28819a0bb8a38779cac08dda9cae08e3e2d68

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat() {
+      cats.pop();
+  }
+
+  function destructivelyRemoveFirstCat() {
+      cats.shift();
+  }
+
+  const newCats = ["Broom"];
+
+  function appendCat(name) {
+      cats.slice(name);
+      let newCats = [...cats, "Broom"];
+      return newCats
+  }
+
+  const newCats2 = ["Arnold"];
+
+  function prependCat(){
+    let newCats2 = ["Arnold",...cats,];
+    return newCats2
+  }
+  
+  function removeFirstCat(){
+  return cats.slice(1);
+  }
+  
+  function removeLastCat(){
+    return cats.slice(0, cats. length - 1);
+  }
